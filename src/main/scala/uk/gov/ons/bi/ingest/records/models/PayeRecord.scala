eec4123de1b559b813b86bd0585b579c773ded42
Flavian Alexandru <flavian@outworkers.com>
2017-02-01 11:20:39 +0000

    Fixing imports for PAYE

--- src/main/scala/uk/gov/ons/bi/ingest/records/models/PayeRecord.scala
+++ src/main/scala/uk/gov/ons/bi/ingest/records/models/PayeRecord.scala
@@ -1,6 +1,7 @@
 package uk.gov.ons.bi.ingest.records.models
 
 import org.joda.time.DateTime
+import uk.gov.ons.bi.ingest.records.parsers._
 
 case class Address(
   line_1: String,
@@ -35,4 +36,42 @@ case class PayeRecord(
   scheme_reopened_date: DateTime
 ) {
   def trade_classification_number: String = legal_status
+}
+
+object PayeRecord {
+  implicit object PayeRecordDelimiter extends OffsetProvider[PayeRecord] {
+    override def parser: OffsetParser = OffsetParser(
+      "emp_stats_rec_id" offset 1 --> 3,
+      "district_number" offset 4 --> 12,
+      "employer_reference" offset 14 --> 17,
+      "accounts_office_reference" offset 18 --> 23,
+      "address_line_1" offset 24 --> 29,
+      "address_line_2" offset 30 --> 30,
+      "address_line_3" offset 30 --> 30,
+      "address_line_4" offset 30 --> 30,
+      "address_line_5" offset 30 --> 30,
+      "postcode" offset 30 --> 30,
+      "trading_as_name_1" offset 31 --> 36,
+      "trading_as_name_2" offset 37 --> 42,
+      "communication_name" offset 43 --> 43,
+      "communication_address_line_1" offset 24 --> 29,
+      "communication_address_line_2" offset 30 --> 30,
+      "communication_address_line_3" offset 30 --> 30,
+      "communication_address_line_4" offset 30 --> 30,
+      "communication_address_line_5" offset 30 --> 30,
+      "communication_postcode" offset 30 --> 30,
+      "legal_status" offset 44 --> 49,
+      "scheme_type" offset 50 --> 50,
+      "date_commenced" offset 51 --> 51,
+      "transfer_in_identifier" offset 52 --> 52,
+      "successor_in_identifier" offset 53 --> 53,
+      "date_ceased" offset 54 --> 54,
+      "transfer_out_identifier" offset 56 --> 56,
+      "merger_out_identifier" offset 57 --> 57,
+      "succession_out_identifier" offset 58 --> 66,
+      "date_of_transfer" offset 67 --> 84,
+      "scheme_cancelled_date" offset 85 --> 189,
+      "scheme_reopened_date" offset 190 --> 219
+    )
+  }
 }
