94f0dd8b3f9750d8517d82f99614fada2aa6bebe
Flavian Alexandru <flavian@outworkers.com>
2017-02-07 15:56:12 +0000

    More parsers

--- models/src/main/scala/uk/gov/ons/bi/ingest/models/CompaniesHouseRecord.scala
+++ models/src/main/scala/uk/gov/ons/bi/ingest/models/CompaniesHouseRecord.scala
@@ -7,8 +7,6 @@ import com.outworkers.util.parsers._
 import com.outworkers.util.validators._
 import uk.gov.ons.bi.ingest.parsers._
 
-import scala.util.Try
-
 case class Accounts(
   accounts_ref_day: String,
   accounts_ref_month: String,
@@ -20,7 +18,7 @@ case class Accounts(
 
 object Accounts {
   implicit object AccountsParser extends CsvParser[Accounts] {
-    override def extract(sourceType: Seq[String]): ValidatedNel[String, Accounts] = {
+    override def extract(sourceType: Seq[String]): Nel[Accounts] = {
       parse[String](sourceType.head).prop("accounts_ref_day") and
         parse[String](sourceType(1)).prop("accounts_ref_month") and
         parseNonEmpty[DateTime](sourceType.value(2)).prop("next_due_date") and
@@ -37,7 +35,7 @@ case class Returns(
 
 object Returns {
   implicit object ReturnsParser extends CsvParser[Returns] {
-    override def extract(sourceType: Seq[String]): ValidatedNel[String, Returns] = {
+    override def extract(sourceType: Seq[String]): Nel[Returns] = {
       parse[DateTime](sourceType.head).prop("next_due_date") and
         parseNonEmpty[DateTime](sourceType.value(1)).prop("last_made_up_date") map (_.as[Returns])
     }
@@ -50,6 +48,18 @@ case class Mortgages(
   num_mort_part_satisfied: Option[Int],
   num_mort_satisfied: Option[Int]
 )
+
+object Mortgages {
+
+  implicit object Mortgages extends CsvParser[Mortgages] {
+    override def extract(sourceType: Seq[String]): Nel[Mortgages] = {
+      parseNonEmpty[Int](sourceType.value(0)).prop("num_mort_charges") and
+        parseNonEmpty[Int](sourceType.value(1)).prop("num_mort_outstanding") and
+        parseNonEmpty[Int](sourceType.value(2)).prop("num_mort_part_satisfied") and
+        parseNonEmpty[Int](sourceType.value(3)).prop("num_mort_satisfied") map (_.as[Mortgages])
+    }
+  }
+}
 
 case class SICCode(
   sic_text_1: String,
